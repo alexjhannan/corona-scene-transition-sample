40e13110174d41d5ffbe89b6c56768c7294b651d
laoyipay <laoyipay@msn.com>
2016-03-02 11:52:59 -0500

    add key control

--- menu-test/engine/controls/controls.lua
+++ menu-test/engine/controls/controls.lua
@@ -13,6 +13,27 @@ function controlWrapper(player)
 	local controlDown = display.newRect(controls, display.viewableContentWidth/5, display.viewableContentHeight/6*4.5 + 25, 25, 25)
 	controlDown:setFillColor(0,0, 1)
 
+	-- KeyBoard Control
+	local function onKeyEvent( event )
+    -- Print which key was pressed down/up
+    local message = "Key '" .. event.keyName .. "' was pressed " .. event.phase
+    print( message )
+
+    -- If the "back" key was pressed on Android or Windows Phone, prevent it from backing out of the app
+    if ( event.keyName == "back" ) then
+        local platformName = system.getInfo( "platformName" )
+        if ( platformName == "Android" ) or ( platformName == "WinPhone" ) then
+            return true
+        end
+    end
+
+    -- IMPORTANT! Return false to indicate that this app is NOT overriding the received key
+    -- This lets the operating system execute its default handling of the key
+    return false
+	end
+	
+
+
 	-- callback functions for controls
 	local function onTapUp() 
 		player.group.y = player.group.y - 10
@@ -36,6 +57,7 @@ function controlWrapper(player)
 	controlRight:addEventListener('tap', onTapRight)
 	controlDown:addEventListener('tap', onTapDown)
 	controlLeft:addEventListener('tap', onTapLeft)
+	Runtime:addEventListener( "key", onKeyEvent )
 
 	-- and return the (now initialized) controls
 	return controls
