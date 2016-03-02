b96db2bc68ef5bf1518df382d01b079abb302976
alexjhannan <alexjhannan@gmail.com>
2016-03-02 14:16:30 -0500

    Removed orientation from config

--- menu-test/config.lua
+++ menu-test/config.lua
@@ -1,14 +1,10 @@
 -- config pattern taken from https://www.youtube.com/watch?v=LZmN1NN71qQ
--- supposedly a good approach to handling multiple devices
+-- supposedly a good approach to handle scaling across multiple devices
 local aspectRatio = display.pixelHeight / display.pixelWidth
 
 application = {
-	orientation =
-    {
-        default = "landscapeRight",
-        supported = {"landscapeRight", "landscapeLeft"}
-    },
 	content = {
+		-- can change static values (320 & 480) to alter global scale (should keep same 2:3 ratio)
 		width = aspectRatio > 1.5 and 320 or math.ceil(480 / aspectRatio),
 		height = aspectRatio < 1.5 and 480 or math.ceil(320 * aspectRatio),
 		scale = "letterBox",
